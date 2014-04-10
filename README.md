065aef7914a0e19531d62ecfa27fd5f9ae1b9b09
Vincent Incarvite <vincent.incarvite@gmail.com>
2014-04-10 00:06:13 -0400

    Add all file except those in node_modules/

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-MyApp
-=====
 
-Basic NodeJS webapp
+
+# MyApp
+
+
+
+## Usage
+
+
+
+## Developing
+- setup project structure
+- setup GIT
+- flesh out db resources
+- look into security issues
+- create user login
+
+
+### Tools
+
+Created with [Nodeclipse](https://github.com/Nodeclipse/nodeclipse-1)
+ ([Eclipse Marketplace](http://marketplace.eclipse.org/content/nodeclipse), [site](http://www.nodeclipse.org))   
+
+Nodeclipse is free open-source project that grows with your contributions.
---
+++ app.js
@@ -0,0 +1,44 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+// Mongo code
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/test_db');
+
+var app = express();
+
+// all environments
+app.set('env', 'development');
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if('development' === app.get('env')){
+	app.use(express.logger('dev'));
+	app.use(express.errorHandler());
+}
+
+// routes
+app.get('/', routes.index);
+app.get('/users', user.list);
+app.get('/userlist', user.listFromMongo(db));
+app.get('/newuser', user.newuser);
+app.post('/adduser', user.adduser(db));
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
