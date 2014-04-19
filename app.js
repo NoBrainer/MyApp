c49f5b3ae276d03e3dd367c3db1f8eb4a757124f
Vincent Incarvite <vincent.incarvite@gmail.com>
2014-04-18 23:24:01 -0400

    Upgraded express to 4.0.0

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 
 // Imports
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
 var express = require('express');
 var http = require('http');
+var methodOverride = require('method-override');
+var mongoose = require('mongoose');
+var morgan = require('morgan');//logger
 var path = require('path');
+var serveStatic = require('serve-static');
+var staticFavicon = require('static-favicon');
 
 // Local imports
 var config = require('./config');
@@ -10,7 +17,6 @@ var routes = require('./routes');
 var user = require('./routes/user');
 
 // Setup the database
-var mongoose = require('mongoose');
 mongoose.connect(config.props.MONGO_LOCATION);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -54,29 +60,31 @@ db.once('open', function(){
 
 // Setup the environment
 var app = express();
-app.set('env', 'development');
-app.set('port', process.env.PORT || config.props.REST_PORT);
-app.set('views', __dirname + '/views');
+var port = process.env.PORT || config.props.PORT;
+app.set('env', config.props.ENV);
+app.set('port', port);
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(staticFavicon());
+app.use(bodyParser());
+app.use(methodOverride());
+app.use(serveStatic(path.join(__dirname, 'public')));
 
 // Do things when in development mode
 if('development' === app.get('env')){
-	app.use(express.logger('dev'));
-	app.use(express.errorHandler());
+	app.use(morgan('dev'));
+	app.use(errorHandler());
 }
 
-// Initialize the routes
-app.get('/api', routes.index);
-app.get('/user/create', user.create);
-app.get('/user/exists', user.exists);
-app.get('/user/getall', user.getAll);
-app.get('/user/update', user.update);
-app.get('/user/delete', user.delete);
+// Initialize the REST routes (TODO:use router.post where applicable)
+var router = express.Router();
+app.use('/api', router);
+router.get('/', routes.index);
+router.get('/user/create', user.create);
+router.get('/user/exists', user.exists);
+router.get('/user/getall', user.getAll);
+router.get('/user/update', user.update);
+router.get('/user/delete', user.delete);
 
 // Start the server
 var server = http.createServer(app);
