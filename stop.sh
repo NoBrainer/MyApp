dc00a43523962997eb991b0c5516397f66404e0f
Vincent Incarvite <vincent.incarvite@gmail.com>
2014-11-22 17:58:29 -0500

    Included file mentioned in last commit

---
+++ stop.sh
@@ -0,0 +1,12 @@
+
+# stop the service
+sudo service copycatco stop
+
+# stop the forever processes
+forever stopall
+
+# stop the process running on port 8080
+fuser -k 8080/tcp
+
+# delete the service pid file
+sudo rm /var/run/CopyCatCo.pid
